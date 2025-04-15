05aceed8fe749c70c2c2324f400f3fc3add9acd5
Gulistane Raza <gulistaneraza01@gmail.com>
2025-04-16 00:41:02 +0530

    hosted on vercel

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -21,8 +21,7 @@ const userClerkWebHooks = async (req, res) => {
           imageUrl: data.image_url,
         };
         await User.create(newUser);
-        res.status(201).json({ success: true, message: "user created" });
-        break;
+        return res.status(201).json({ success: true, message: "user created" });
       }
 
       case "user.updated": {
@@ -32,68 +31,18 @@ const userClerkWebHooks = async (req, res) => {
           imageUrl: data.image_url,
         };
         await User.findByIdAndUpdate(data.id, updateUser);
-        res.json({ success: true, message: "user updated" });
-        break;
+        return res.json({ success: true, message: "user updated" });
       }
 
       case "user.deleted": {
         await User.findByIdAndDelete(data.id);
-        res.json({ success: true, message: "user deleted" });
-        break;
+        return res.json({ success: true, message: "user deleted" });
       }
-
-      default:
-        break;
     }
-
-    console.log({ a: req.headers, b: req.body });
-    return res.json({ success: true });
   } catch (error) {
     console.log(error.message);
     return res.status(400).json({ success: false, message: error.message });
   }
-  // const whook = new Webhook(clerkWebhookSecret);
-  // try {
-  //   await whook.verify(req.body, req.headers);
-
-  //   const { data, action } = req.body;
-
-  //   switch (action) {
-  //     case "user.created": {
-  //       const _id = data.id;
-  //       const name = `${data.first_name} ${data.last_name}`;
-  //       const email = data.email_addresses[0].email_address;
-  //       const imageUrl = data.image_url;
-  //       const newUser = { _id, name, email, imageUrl };
-  //       await User.create(newUser);
-  //       res.json({ success: true, message: "user created" });
-  //       break;
-  //     }
-
-  //     case "user.updated": {
-  //       const name = `${data.first_name} ${data.last_name}`;
-  //       const email = data.email_addresses[0].email_address;
-  //       const imageUrl = data.image_url;
-  //       const updateUser = { name, email, imageUrl };
-  //       await User.findByIdAndUpdate(data.id, updateUser);
-  //       res.json({ success: true, message: "user updated" });
-  //       break;
-  //     }
-
-  //     case "user.deleted": {
-  //       await User.findByIdAndDelete(data.id);
-  //       res.json({ success: true, message: "user deleted" });
-  //       break;
-  //     }
-
-  //     default:
-  //       break;
-  //   }
-
-  //   return res.json({ header: req.headers, body: req.body, success: true });
-  // } catch (error) {
-  //   return res.status(400).json({ success: false, message: "Invalid webhook" });
-  // }
 };
 
 export { userClerkWebHooks };
