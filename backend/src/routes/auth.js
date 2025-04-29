f95d3fe621ecb308be4e8174205caf20002cfe60
Gulistane Raza <gulistaneraza01@gmail.com>
2025-04-30 00:51:46 +0530

    hosted in vercel

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -4,6 +4,6 @@ import { userClerkWebHooks, getUser } from "../controllers/auth.js";
 const router = Router();
 
 router.get("/getuser", getUser);
-router.post("/userclerkwebhooks", userClerkWebHooks);
+router.post("/userclerk", userClerkWebHooks);
 
 export default router;
